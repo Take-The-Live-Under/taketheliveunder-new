abe9a7950296a94612dead9b5ed600e910c989c4
Arboy Magomba <arboydev@gmail.com>
2026-03-11 18:20:45 -0500

    Pause ESLint & TypeScript checking for product-site

--- apps/product-site/next.config.js
+++ apps/product-site/next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
